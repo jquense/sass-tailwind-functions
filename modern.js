46544f9a5c260d59d1884dd7adb1419c1bf159f1
Dylan Awalt-Conley <web@dylan.ac>
2023-03-02 12:57:05 -0500

    refactor: replace modern api wrapper interface with modern api functions

--- modern.js
+++ modern.js
@@ -1,25 +1,104 @@
-const { fromSass } = require('sass-cast');
-const { toSass } = require('sass-cast/legacy');
+// @ts-expect-error no types
+const toPath = require('lodash/toPath');
+const { toSass, fromSass } = require('sass-cast');
+const buildMediaQuery =
+  require('tailwindcss/lib/util/buildMediaQuery').default;
 const escapeClassName =
   require('tailwindcss/lib/util/escapeClassName').default;
+const resolveConfig = require('tailwindcss/resolveConfig');
 
-const sassThemeFn = require('./index');
-
-const toLegacy = (sassValue) =>
-  toSass(fromSass(sassValue), {
-    parseUnquotedStrings: true,
-    quotes: null,
-  });
-const toModern = (sassValue) => sassValue.dartValue ?? sassValue;
+const EMPTY = '@@EMPTY@@';
 
 module.exports = (sass, tailwindConfig) => {
-  const functions = sassThemeFn(sass, tailwindConfig);
-  const [[themeFnKey, themeFn], [screenFnKey, screenFn], [escapeFnKey]] =
-    Object.entries(functions);
-  const newFunctions = {
-    [themeFnKey]: (args) => toModern(themeFn(...args.map(toLegacy))),
-    [screenFnKey]: (args) => toModern(screenFn(...args.map(toLegacy))),
-    [escapeFnKey]: ([str]) => new sass.SassString(escapeClassName(str.text)),
+  const { theme } = resolveConfig(require(tailwindConfig));
+
+  const themeTransforms = {
+    fontSize(value) {
+      return Array.isArray(value) ? value[0] : value;
+    },
+    outline(value) {
+      return Array.isArray(value) ? value[0] : value;
+    },
   };
-  return newFunctions;
+
+  const themeFn = ([$keys, $dflt, $listSep]) => {
+    const keys = fromSass($keys);
+    const hasDefault = fromSass($dflt) !== EMPTY;
+    const listSep = $listSep.assertString('$list-separator').text;
+
+    let path;
+    if (Array.isArray(keys)) {
+      path = keys;
+    } else {
+      $keys.assertString('$keys');
+      path = toPath(keys);
+    }
+
+    let current;
+    let itemValue = theme;
+    const pristinePath = [...path];
+    const themeSection = pristinePath[0];
+
+    const transform = themeTransforms[themeSection];
+
+    while (itemValue && path.length) {
+      current = path.shift();
+
+      if (typeof itemValue === 'object' && itemValue) {
+        itemValue = Array.isArray(itemValue)
+          ? itemValue[+current]
+          : itemValue[current];
+
+        if (itemValue === undefined) {
+          if (hasDefault) return $dflt;
+
+          throw new Error(
+            `There is no theme value at ${pristinePath
+              .slice(0, pristinePath.indexOf(current) + 1)
+              .join('.')}`,
+          );
+        }
+      } else {
+        if (hasDefault) return $dflt;
+        throw new Error(
+          `The value at ${pristinePath
+            .slice(0, pristinePath.indexOf(current))
+            .join(
+              '.',
+            )} is not an object or array, and has no key: ${current}.`,
+        );
+      }
+    }
+
+    if (transform) {
+      itemValue = transform(itemValue);
+    }
+
+    if (itemValue == null) {
+      return $dflt;
+    }
+
+    let result = toSass(itemValue, {
+      parseUnquotedStrings: true,
+      quotes: false,
+    });
+    if (result.separator && result.separator !== listSep)
+      result = new sass.SassList(result.asList, { separator: listSep });
+
+    return result;
+  };
+
+  function screenFn([$screen]) {
+    const screen = $screen.assertString('$screen').text;
+    if (theme.screens[screen] === undefined) {
+      throw new Error(`The '${screen}' screen does not exist in your theme.`);
+    }
+    return new sass.SassString(buildMediaQuery(theme.screens[screen]));
+  }
+
+  return {
+    [`theme($keys, $defaultValue: "${EMPTY}", $list-separator: ",")`]: themeFn,
+    'screen($screen)': screenFn,
+    'e($str)': ([str]) => new sass.SassString(escapeClassName(str.text)),
+  };
 };
